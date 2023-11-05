08789e7154dc7e37d4653bb7affca61bdd886289
Wei <mai@mai0313.com>
2023-11-06 07:15:15 +0800

    Add blank line to test_example function

--- tests/test_example.py
+++ tests/test_example.py
@@ -3,6 +3,7 @@
 The variables in `conftest.py` are available in this file or all test_* files.
 """
 
+
 def test_example(get_variables):
     """This will get variables from another pytest, `conftest.py`."""
     assert isinstance(get_variables, int)
