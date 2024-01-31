8041fdcb651d1b08fb124a36a4d93dc7897e2210
Wei <21039366+Mai0313@users.noreply.github.com>
2024-01-31 10:37:47 +0800

    update actions (#31)
    * update actions
    
    * add vscode related
    
    * Update .pre-commit-config.yaml
    
    * Update labeler.yml
    
    * fix: replace labeler format
    
    * fix: labeler formating
    
    * fix: replace GH_TOKEN to GT_TOKEN
    
    * docs: add readme for actions
    
    * fix conflict
    
    * fix repo token
    
    * feat: add more labels
    
    * style: change to list, remove duplicates
    
    * style: move todo to issue out
    
    * pre-commit
    
    * test todo-to-issue

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,10 @@
-# --------------------------------------------------------
-# Repo Template
-# Copyright (c) 2023 Mediatek.inc
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Wei (mtk30765)
-# --------------------------------------------------------
-
 import pytest
 
 
 @pytest.fixture(scope="session", autouse=True)
 def get_nums() -> int:
-    """This is a sample of how you can send variables to another pytest."""
+    """This is a sample of how you can send variables to another pytest.
+
+    TODO: This is a sample that shows how to send variables to another pytest (also test todo-to-issue action).
+    """
     return 500
--- tests/test_example.py
+++ tests/test_example.py
@@ -1,13 +1,3 @@
-# --------------------------------------------------------
-# Repo Template
-# Copyright (c) 2023 Mediatek.inc
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Wei (mtk30765)
-# --------------------------------------------------------
-
-from typing import Any
-
-
 def test_example(get_nums: int) -> None:
     """This will get variables from another pytest, `conftest.py`."""
     assert isinstance(get_nums, int)
