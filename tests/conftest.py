cd3a290a9d264266f751334172486f5dfe3f826a
Wei <21039366+Mai0313@users.noreply.github.com>
2024-01-31 02:36:26 +0000

    test todo-to-issue

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,5 +3,8 @@ import pytest
 
 @pytest.fixture(scope="session", autouse=True)
 def get_nums() -> int:
-    """This is a sample of how you can send variables to another pytest."""
+    """This is a sample of how you can send variables to another pytest.
+
+    TODO: This is a sample that shows how to send variables to another pytest (also test todo-to-issue action).
+    """
     return 500
