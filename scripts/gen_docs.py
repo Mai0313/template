3a1752066ea3d80970d27ca0f7c8cb1706f6ae16
mtk30765 <wei.chenglee@mediatek.com>
2024-11-15 11:13:49 +0800

    chore: [] migrate to Rye and update documentation setup
    - Comment out the Python setup step and replace it with Rye setup in the GitHub Actions workflow
    - Change the pip install commands to use Rye in the GitHub Actions workflow
    - Add new markdown files for documentation references
    - Update pip installation instructions to use `requirements.lock` instead of `requirements.txt`
    - Exclude `model_` files from the documentation build in `mkdocs.yml`
    - Remove `fastapi` dependency from `pyproject.toml`
    - Update documentation generation commands and add new chains in `pyproject.toml`
    - Add new development dependencies and update versions of existing ones in `pyproject.toml`
    - Change project name from `tma_api` to `template` in `pyproject.toml`
    - Simplify mypy exclude patterns in `pyproject.toml`
    - Delete `requirements.txt` file
    - Add new imports and docstrings to `scripts/gen_docs.py`
    - Refactor `DocsGenerator` class to support asynchronous operations and add new methods in `scripts/gen_docs.py`
    - Enhance `Response` model with additional field attributes and configurations in `src/template/types/response.py`
    
    Signed-off-by: mtk30765 <wei.chenglee@mediatek.com>

--- scripts/gen_docs.py
+++ scripts/gen_docs.py
@@ -1,11 +1,40 @@
+import ast
 import shutil
+from typing import Literal
 from pathlib import Path
 
+import anyio
 from pydantic import Field, BaseModel, ConfigDict, computed_field
 from rich.progress import Progress
 
 
 class DocsGenerator(BaseModel):
+    """DocsGenerator is a class that generates documentation for Python files or classes within a specified source directory.
+
+    Attributes:
+        source (str): The source directory or file path.
+        output (str): The output directory path.
+        exclude (str): Comma-separated list of folders or files to exclude.
+        mode (Literal["file", "class"]): Mode of documentation generation, either by file or class.
+
+    Methods:
+        gen_docs() -> None:
+            Generates documentation by file or class.
+
+        __call__() -> None:
+            Asynchronously calls the gen_docs method.
+
+    Using CLI:
+        ```bash
+        python ./scripts/gen_docs.py --source ./src --output ./docs/Reference --exclude .venv gen_docs
+        ```
+
+    Using Rye:
+        ```bash
+        rye run gen
+        ```
+    """
+
     model_config = ConfigDict(arbitrary_types_allowed=True)
     source: str = Field(..., frozen=True)
     output: str = Field(..., frozen=True)
@@ -14,36 +43,112 @@ class DocsGenerator(BaseModel):
         description="Exclude the folder or file, it should be separated by comma.",
         examples=[".venv,.git,.idea"],
     )
+    mode: Literal["file", "class"] = Field(
+        default="class", description="Generate docs by file or class."
+    )
 
     @computed_field
     @property
     def _source_path(self) -> Path:
+        """Computed property that returns the source path as a Path object.
+
+        Returns:
+            Path: The source path.
+        """
         return Path(self.source)
 
     @computed_field
     @property
     def _output_path(self) -> Path:
+        """Computed property that returns the output path as a Path object.
+
+        Returns:
+            Path: The output path.
+        """
         return Path(self.output)
 
-    @staticmethod
-    def __gen_single_docs(docs_path: Path, file: Path) -> str:
+    async def __gen_content(self, file: Path) -> str:
+        """Generates documentation content based on the specified mode.
+
+        Args:
+            file (Path): The file path for which the documentation content is to be generated.
+
+        Returns:
+            str: The generated documentation content as a string.
+
+        Raises:
+            ValueError: If the mode is not "file" or "class".
+
+        Notes:
+            - If the mode is "file", the function generates a string with the file path formatted for documentation.
+            - If the mode is "class", the function reads the file, parses its AST, and generates a string with class names formatted for documentation, excluding classes with names starting with an underscore.
+            - If no content is generated, a default string with the file path formatted for documentation is returned.
+        """
+        if self.mode == "file":
+            note_content = f"::: {file.as_posix().removesuffix('.py').replace('/', '.')}\n"
+        elif self.mode == "class":
+            async with await anyio.open_file(file, encoding="utf-8") as f:
+                contents = await f.read()
+                tree = ast.parse(source=contents, filename=file)
+
+            note_content = ""
+            for node in ast.walk(tree):
+                if isinstance(node, ast.ClassDef):
+                    if node.name.startswith("_"):
+                        continue
+                    note_content += f"::: {file.as_posix().removesuffix('.py').replace('/', '.')}.{node.name}\n"
+        else:
+            raise ValueError("Invalid mode")
+        if not note_content:
+            note_content = f"::: {file.as_posix().removesuffix('.py').replace('/', '.')}\n"
+        return note_content
+
+    async def __gen_single_docs(self, docs_path: Path, file: Path) -> str:
+        """Generates documentation for a single file.
+
+        This asynchronous method generates documentation for a given file and saves it as a markdown file in the specified documentation path.
+        It ensures that the output directory exists and removes any existing file with the same name before writing the new content.
+
+        Args:
+            docs_path (Path): The path where the documentation should be saved.
+            file (Path): The file for which the documentation is to be generated.
+
+        Returns:
+            str: The path to the generated documentation file as a string.
+        """
         _output_docs_path = Path(docs_path / file.with_suffix(".md").name)
         _output_docs_path.parent.mkdir(parents=True, exist_ok=True)
         _output_docs_path.unlink(missing_ok=True)
-        _note_content = f"::: {file.as_posix().removesuffix('.py').replace('/', '.')}\n"
+        _note_content = await self.__gen_content(file=file)
         _output_docs_path.write_text(data=_note_content, encoding="utf-8")
         return _output_docs_path.as_posix()
 
-    def __remove_existing_folder(self) -> None:
+    async def __remove_existing_folder(self) -> None:
+        """Asynchronously removes the existing folder at the output path if it exists, and then recreates the folder.
+
+        This method checks if the output path exists. If it does, the folder and its contents are removed. After removal, a new folder is created at the same path.
+        """
         if self._output_path.exists():
             shutil.rmtree(self._output_path.absolute())
             self._output_path.mkdir(parents=True, exist_ok=True)
 
-    def gen_docs(self) -> None:
+    async def gen_docs(self) -> None:
+        """This function can generate docs by file or class.
+
+        Raises:
+            ValueError: If the source path is invalid.
+
+        Examples:
+            >>> import asyncio
+            >>> pair_list = {"./src": "./docs/Reference"}
+            >>> for key, value in pair_list.items():
+            ...     docs_generator = DocsGenerator(source=key, output=value, exclude=".venv", mode="class")
+            ...     asyncio.run(docs_generator.gen_docs())
+        """
         with Progress() as progress:
             task = progress.add_task("[green]Generating docs...")
             if self._source_path.is_dir():
-                self.__remove_existing_folder()
+                await self.__remove_existing_folder()
 
                 need_to_exclude = [*self.exclude.split(","), "__init__.py"]
                 files = self._source_path.glob("**/*.py")
@@ -59,7 +164,7 @@ class DocsGenerator(BaseModel):
                     docs_path = Path(
                         f"{self._output_path}/{file.parent.relative_to(self._source_path)}"
                     )
-                    processed_file = self.__gen_single_docs(docs_path=docs_path, file=file)
+                    processed_file = await self.__gen_single_docs(docs_path=docs_path, file=file)
                     progress.update(
                         task_id=task,
                         advance=1,
@@ -69,7 +174,7 @@ class DocsGenerator(BaseModel):
 
             elif self._source_path.is_file():
                 progress.update(task_id=task, description="[cyan]Files Found...", total=1)
-                processed_file = self.__gen_single_docs(self._output_path, self._source_path)
+                processed_file = await self.__gen_single_docs(self._output_path, self._source_path)
                 progress.update(
                     task_id=task,
                     advance=1,
@@ -79,18 +184,22 @@ class DocsGenerator(BaseModel):
             else:
                 raise ValueError("Invalid source path")
 
+    async def __call__(self) -> None:
+        """Asynchronously calls the gen_docs method.
+
+        This method is intended to be used as a callable object, which when called, will execute the gen_docs method asynchronously.
+
+        Examples:
+            >>> import asyncio
+            >>> pair_list = {"./src": "./docs/Reference"}
+            >>> for key, value in pair_list.items():
+            ...     docs_generator = DocsGenerator(source=key, output=value, exclude=".venv", mode="class")
+            ...     asyncio.run(docs_generator())
+        """
+        await self.gen_docs()
+
 
 if __name__ == "__main__":
     import fire
 
     fire.Fire(DocsGenerator)
-
-    # pair_list = {
-    #     "./app": "./docs/API",
-    #     "./src": "./docs/Dashboard",
-    #     "./pages": "./docs/Page",
-    #     "./api.py": "./docs/API",
-    # }
-    # for key, value in pair_list.items():
-    #     docs_generator = DocsGenerator(source_path=key, docs_path=value)
-    #     docs_generator.gen_docs()
